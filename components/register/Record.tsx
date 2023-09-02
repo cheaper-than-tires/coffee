ec04a109c7dbcb733a20db949df92b46f1067af0
gimhanul <gimhanuly@gmail.com>
2023-09-02 18:23:32 +0900

    feat: record

--- components/register/Record.tsx
+++ components/register/Record.tsx
@@ -1,10 +1,19 @@
 import styled from '@emotion/styled';
-import { Column, Row } from '@/components/common/Flex';
-import { ChangeEventHandler } from 'react';
+import { Row } from '@/components/common/Flex';
+import { ChangeEventHandler, useCallback, useState } from 'react';
+import { AudioRecorder } from 'react-audio-voice-recorder';
 
 export default function Record({ onChange }: {
   onChange: ChangeEventHandler<HTMLInputElement>
 }) {
+  const addAudioElement = (blob: Blob) => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement('audio');
+    audio.src = url;
+    audio.controls = true;
+    document.body.appendChild(audio);
+  };
+
   return (
     <>
       <LoadFileContainer>
@@ -12,6 +21,26 @@ export default function Record({ onChange }: {
       </LoadFileContainer>
       <RecordContainer>
         <p>녹음하기</p>
+        <AudioRecorder
+          onRecordingComplete={addAudioElement}
+          audioTrackConstraints={{
+            noiseSuppression: true,
+            echoCancellation: true,
+            // autoGainControl,
+            // channelCount,
+            // deviceId,
+            // groupId,
+            // sampleRate,
+            // sampleSize,
+          }}
+          onNotAllowedOrFound={(err) => console.table(err)}
+          downloadOnSavePress={true}
+          downloadFileExtension="webm"
+          mediaRecorderOptions={{
+            audioBitsPerSecond: 128000,
+          }}
+          // showVisualizer={true}
+        />
       </RecordContainer>
     </>
   )
